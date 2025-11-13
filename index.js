4b65fe74de12db7766d5f2f06eb72abe724c2360
Aakash Kavediya <kavediyaaakash@gmail.com>
2025-11-14 00:04:04 +0530

    change in port

--- index.js
+++ index.js
@@ -97,5 +97,5 @@ app.post(
 );
 
 
-const PORT = 8000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, "0.0.0.0", () => console.log(`Server running on port ${PORT}`));
